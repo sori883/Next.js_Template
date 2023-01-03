7e58f6adb70e383c15b8a0f4e7763459f0bd8d94
sorinaji <37400002+sori883@users.noreply.github.com>
2023-01-04 00:54:24 +0900

    v1

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Next.js_Template
+This is a [Next.js](https://nextjs.org/) project template.
+
+## Usage
+1. Create new Repository from [this template](https://github.com/sori883/Next.js_Template/generate)
+
+1. Git Clone 
+
+1. Rename env.local.example to env.local
+
+1. Execute the following commands
+```
+$ yarn install
+$ yarn dev 
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## Includes
+- emotion
+- mui
+  - see: sec/mui
+- next-head-seo
+  - see: src/components/nonVisual/siteHead
+- storybook
+  - see: .storybook
+- husky
+  - added eslint
+- pathpida
+- next-sitemap
+  - see: next-sitemap.config.js
+
+## env
+- APP_URL
+  - Used for these
+    - next.config.js
+    - src/components/nonVisual/siteHead
+    - next-sitemap.config.js
---
+++ src/pages/_app.tsx
@@ -0,0 +1,36 @@
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import type { AppProps } from 'next/app';
+
+import { SiteHead } from 'components/nonVisual/siteHead';
+import createEmotionCache from 'mui/createEmotionCache';
+import theme from 'mui/theme';
+
+import 'styles/globals.css';
+
+const clientSideEmotionCache = createEmotionCache();
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+function AppInit():null {
+  return null;
+}
+
+function MyApp(props: MyAppProps): JSX.Element {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <SiteHead />
+          <Component {...pageProps} />
+          <AppInit />
+        </ThemeProvider>
+      </CacheProvider>
+    </>
+  );
+}
+export default MyApp;
